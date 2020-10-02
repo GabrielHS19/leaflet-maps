170e4e856044826282a0a0328cecbf61545a2ec3
GabrielHS19 <jesus.gh72@gmail.com>
2020-10-01 21:03:14 -0500

    update de mapa

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,26 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
+import { icon, Marker } from 'leaflet';
+import { MarkerService } from '../_services/marker.service';
+import { ShapeService } from '../_services/shape.service';
+
+
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 
 @Component({
   selector: 'app-map',
@@ -8,16 +29,73 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   private map;
+  private states;
 
-  constructor() { }
+  constructor(private markerService: MarkerService, 
+    private shapeService: ShapeService) {
+  }
+
 
   ngAfterViewInit(): void {
     this.initMap();
+    //this.markerService.makeCapitalMarkers(this.map);
+    
+    this.shapeService.getStateShapes().subscribe(states => {
+      this.states = states;
+      this.initStatesLayer();
+      this.markerService.makeCapitalCircleMarkers(this.map);
+    });
   }
+
+  private initStatesLayer() {
+    const stateLayer = L.geoJSON(this.states, {
+      style: (feature) => ({
+        weight: 3,
+        opacity: 0.3,
+        color: '#008f68',
+        fillOpacity: 0.8,
+        fillColor: '#6DB65B'
+      }),
+      onEachFeature: (feature, layer) => (
+        layer.on({
+          mouseover: (e) => (this.highlightFeature(e)),
+          mouseout: (e) => (this.resetFeature(e)),
+    })
+      )
+  });
+
+    this.map.addLayer(stateLayer);
+  }
+
+
+  private highlightFeature(e)  {
+    const layer = e.target;
+    layer.setStyle({
+      weight: 10,
+      opacity: 1.0,
+      color: '#DFA612',
+      fillOpacity: 1.0,
+      fillColor: '#FAE042',
+    });
+  }
+
+  private resetFeature(e)  {
+    const layer = e.target;
+    layer.setStyle({
+      weight: 3,
+      opacity: 0.5,
+      color: '#008f68',
+      fillOpacity: 0.8,
+      fillColor: '#6DB65B'
+    });
+  }
+
+
+
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 1.85, 42.1 ],
-      zoom: 20
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
